ef065eef8ea4674445ec11a698a136da365fd030
Nicole Lam <Nicolelam8891@gmail.com>
2023-05-17 14:56:30 -0600

    Add destination array

--- friends.js
+++ friends.js
@@ -2,3 +2,7 @@ var arrays = ["Deanna", "Gavin", "Ben", "Kim", "Kaitlynn"]
     console.log(arrays [3]);
     console.log(arrays [4]); 
 console.log(arrays [0]); 
+
+var destinations = ["Spain", "China", "Brazil", "Africa", "Mexico"]
+destinations.pop();
+console.log(destinations);
